cf12156553303ac6e60370ff18f236f47e52ab45
Martin Joly <snwfdhmp@gmail.com>
2021-03-22 23:03:15 +0100

    fix empty line bug

--- dnsrecon.sh
+++ dnsrecon.sh
@@ -1,7 +1,7 @@
 cwd=$(dirname $0)
 
 try() {
-	if [ "$(dig +short $1 | wc -l | xargs)" -gt 0 ]; then
+	if [ "$(dig +short $1 2>/dev/null | wc -l | xargs)" -gt 0 ]; then
 		echo $1
 	fi
 }
@@ -9,5 +9,7 @@ try() {
 domain=$1
 try $domain
 for prefix in $(cat $cwd/dict.txt); do
+	[ -z $prefix ] && continue
+
 	try $prefix.$domain
 done
